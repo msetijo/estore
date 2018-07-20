0070ad0e1811f93e4e33fd7ce1219c22658ce76e
Mel S <msetijo@users.noreply.github.com>
2018-07-20 15:32:11 -0400

    update

--- js/q3.js
+++ js/q3.js
@@ -1,11 +1,31 @@
+
 $( document ).ready(function() {
-	
+
 	$(".regular").slick({
+		arrows: false,
 		dots: true,
 		infinite: true,
 		slidesToShow: 2.8,
-		slidesToScroll: 1
-	  });
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 2
+			}
+		}, {
+			breakpoint: 820,
+			settings: {
+				slidesToShow: 1
+			}
+		}, {
+			breakpoint: 565,
+			settings: {
+				slidesToShow: 1,
+				arrows: false,
+				infinite: true
+			}
+		}]
+	});
 
     $( "nav li a" ).click(function() {
 		$("nav li a.active").removeClass("active");
